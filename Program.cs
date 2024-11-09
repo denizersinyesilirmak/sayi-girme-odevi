e0cb4d3c30d966d74bc03a2872cb5489009d0335
Deniz Ersin Yeşilirmak <denizersinyesilirmak@gmail.com>
2024-11-09 14:54:05 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int toplam = 0;
+        int pozitifSayilar = 0;
+        int sayi;
+
+        Console.Write("sayı girin: ");
+        sayi = int.Parse(Console.ReadLine());
+
+        while (sayi == 0)
+        {
+            Console.Write("0 dışında bir sayı girin: ");
+            sayi = int.Parse(Console.ReadLine());
+        }
+
+        while (true)
+        {
+            if (sayi < 0)
+            {
+                Console.WriteLine("Pozitif Sayı Girin!!");
+            }
+            else if (sayi > 0)
+            {
+                toplam += sayi;
+                pozitifSayilar++;
+            }
+
+            if (sayi == 0)
+            {
+                if (pozitifSayilar > 0)
+                {
+                    double ortalama = (double)toplam / pozitifSayilar;
+                    Console.WriteLine($"Toplam: {toplam}");
+                    Console.WriteLine($"Ortalama: {ortalama}");
+                }
+                else
+                {
+                    Console.WriteLine("Hiç pozitif sayı girilmedi.");
+                }
+                return;
+            }
+
+            Console.Write("sayı girin: ");
+            sayi = int.Parse(Console.ReadLine());
+        }
+    }
+}
